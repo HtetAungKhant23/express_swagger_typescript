278973ca923b4fca3a43d3453870eb1ae787aaf4
htetaungkhant <htetaung23.ha@gmail.com>
2023-09-14 14:39:10 +0630

    added: using tsoa framework for swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import express, { Application } from "express";
+import router from "./routes/pingRoute";
+import swaggerUi from "swagger-ui-express";
 
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
 
-app.get("/ping", async (_req, res) => {
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", async (_req, res) => {
   res.send({
-    message: "hello",
+    message: "hello world",
   });
 });
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
